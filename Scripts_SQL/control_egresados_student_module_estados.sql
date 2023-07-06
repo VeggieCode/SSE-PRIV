d2ed3e423c4a020f496b7cd9addda33c8c83db9f
KariVI <gcvaldesiglesiaskarina@gmail.com>
2023-07-06 10:41:11 -0600

    Especificaciones del despliegue

--- Scripts_SQL/control_egresados_student_module_estados.sql
+++ Scripts_SQL/control_egresados_student_module_estados.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `student_module_estados`
 --
-
+USE control_egresados;
 DROP TABLE IF EXISTS `student_module_estados`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
--- Scripts_SQL/control_egresados_student_module_municipios.sql
+++ Scripts_SQL/control_egresados_student_module_municipios.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `student_module_municipios`
 --
-
+USE control_egresados;
 DROP TABLE IF EXISTS `student_module_municipios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
