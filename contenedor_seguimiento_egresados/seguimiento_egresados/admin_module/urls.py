e5ec16f614b79e5f2d23fa2a24e11fa2bbd01647
veggiecode <alexisao@hotmail.com>
2023-12-12 12:17:24 -0600

    PEP8 correccion de estilos

--- contenedor_seguimiento_egresados/seguimiento_egresados/admin_module/urls.py
+++ contenedor_seguimiento_egresados/seguimiento_egresados/admin_module/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 app_name = 'admin_module'
 
 urlpatterns = [
-    path('%sloginadmin/' % settings.PATH_PREFIX, CustomLoginView.as_view(template_name="admin_module/login.html"), name="loginadmin"),
+    path('%sloginadmin/' % settings.PATH_PREFIX, CustomLoginView.as_view(template_name="admin_module/login.html"),
+         name="loginadmin"),
     path('%slogout_view/' % settings.PATH_PREFIX, logout_view, name='logout'),
     path('%shomeadmin/' % settings.PATH_PREFIX, home, name='home'),
     path('%sstudents/<str:enrollment>/' % settings.PATH_PREFIX, detail_student, name='student_detail'),
