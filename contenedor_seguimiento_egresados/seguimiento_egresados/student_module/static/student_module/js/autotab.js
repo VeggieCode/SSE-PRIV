fa8b239f07219d5e38c5a715ce20d31f9fa2841e
Alexis Alvarez <alexisao@hotmail.com>
2024-05-27 11:19:14 -0600

    Student_module static files moved to recommended structure "app/static/app"

---
+++ contenedor_seguimiento_egresados/seguimiento_egresados/student_module/static/student_module/js/autotab.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('input.autotab').autotab({ format: 'custom', pattern: 's9', maxlength: 9 });
+});
+
+$(document).ready(function() {
+    $("#id_username_1, #id_username_2, #id_username_3, #id_username_4, #id_username_5, #id_username_6, #id_username_7, #id_username_8").on("keydown", function(e) {
+        var key = e.which || e.keyCode;
+        if ((key < 48 || key > 57) && key !== 8) {
+            e.preventDefault();
+        }
+    });
+
+    $("#id_username_0, #id_username_1, #id_username_2, #id_username_3, #id_username_4, #id_username_5, #id_username_6, #id_username_7, #id_username_8").on("keyup", function(e) {
+        var index = $(this).attr('maxlength');
+        if (this.value.length === this.maxLength) {
+            $(this).next(":input").focus();
+        }
+        else if (e.keyCode === 8 && this.value.length === 0) {
+            $(this).prev(":input").focus();
+        }
+    });
+});
