b8f6f368d146d2da389aae3ab8a2200a224fcf5a
Alexis Alvarez <alexisao@hotmail.com>
2024-06-04 14:59:15 -0600

    Normalizing templates location on student_module and main app

--- contenedor_seguimiento_egresados/seguimiento_egresados/seguimiento_egresados/views.py
+++ contenedor_seguimiento_egresados/seguimiento_egresados/seguimiento_egresados/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
-def custom_error_view(request, exception=None):
-    return render(request, '500.html', status=500)
-def error_404_view(request, exception):
+
+def custom_error_view(request):
+    return render(request, 'seguimiento_egresados/500.html', status=500)
+
+
+def error_404_view(request):
     response = {
         'status': 404,
         'message': 'No se encontró la página que buscaba',
     }
-    return render(request, template_name='404.html', context=response)
+    return render(request, template_name='seguimiento_egresados/404.html', context=response)
