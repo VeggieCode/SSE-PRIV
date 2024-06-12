5a6775f46c2d333fbd52e2ec81e51c09800b481e
Alexis Alvarez <alexisao@hotmail.com>
2024-06-12 16:06:35 -0600

    student_info.html fix due hardcode of the municios endpoint on municipios.js script. Now the url is generated and passed to the template with the url of the endpoint and prefix

--- contenedor_seguimiento_egresados/seguimiento_egresados/student_module/static/student_module/js/municipios.js
+++ contenedor_seguimiento_egresados/seguimiento_egresados/student_module/static/student_module/js/municipios.js
@@ -1,29 +1,28 @@
-$(document).ready(function() {
+/** The variable municipiosEndpoint should be defined in script tag inside the template that is using this script. */
+$(document).ready(function () {
+    // Obtener una referencia a los elementos del DOM
     var selectEstado = document.getElementById("estado");
-    let prefix = '/sse'
     var selectMunicipio = document.getElementById("municipio");
     // Cuando se selecciona un estado, cargar los municipios correspondientes
-    selectEstado.addEventListener("change", function() {
-        var estadoId = this.value;
+    selectEstado.addEventListener("change", function () {
+        let estadoId = this.value;
         if (estadoId) {
-        let url = `${prefix}/municipios/${estadoId}/`;
+            const url = municipiosEndpoint.replace('0', estadoId)
+            fetch(url)
+                .then(response => response.json())
+                .then(municipios => {
+                    selectMunicipio.innerHTML = "<option value=''>Selecciona un municipio</option>";
+                    municipios.forEach(municipio => {
+                        var option = document.createElement("option");
+                        option.value = municipio.id;
+                        option.text = municipio.nombre;
+                        selectMunicipio.add(option);
+                    });
 
-        fetch(url)
-        .then(response => response.json())
-        .then(municipios => {
+                });
+        } else {
+            // Si no se selecciona ningún estado, vaciar los municipios
             selectMunicipio.innerHTML = "<option value=''>Selecciona un municipio</option>";
-            municipios.forEach(municipio => {
-            var option = document.createElement("option");
-            option.value = municipio.id;
-            option.text = municipio.nombre;
-            selectMunicipio.add(option);
-            });
-
-        });
-    } else {
-        // Si no se selecciona ningún estado, vaciar los municipios
-        selectMunicipio.innerHTML = "<option value=''>Selecciona un municipio</option>";
-
-    }
+        }
     });
 });
