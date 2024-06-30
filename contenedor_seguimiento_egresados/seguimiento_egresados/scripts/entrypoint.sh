147e6b75fbf257a87aa078e8b66fb5c19948a336
Alexis Alvarez <alexisao@hotmail.com>
2024-06-30 16:34:11 -0600

    Setting python version explicitly on entrypoint.sh

--- contenedor_seguimiento_egresados/seguimiento_egresados/scripts/entrypoint.sh
+++ contenedor_seguimiento_egresados/seguimiento_egresados/scripts/entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-python -u manage.py wait_for_db
-python -u manage.py collectstatic --noinput
-python -u manage.py migrate
+python3 -u manage.py wait_for_db
+python3 -u manage.py collectstatic --noinput
+python3 -u manage.py makemigrations
+python3 -u manage.py migrate
 
 gunicorn --bind :8000 seguimiento_egresados.wsgi:application --reload
